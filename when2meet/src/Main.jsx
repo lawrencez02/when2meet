980ae6ad14ab3577f0f64f573112b9195cb2dadb
Lawrence Zhang <lawrencezhang@college.harvard.edu>
2022-09-26 12:18:19 -0400

    Add comments

--- when2meet/src/Main.jsx
+++ when2meet/src/Main.jsx
@@ -1,15 +1,26 @@
+// import React and the ScheduleSelector (When2Meet) component from the package
 import React from "react";
 import ScheduleSelector from "react-schedule-selector";
 
+// define the a new class Main that contains the ScheduleSelector component and its state
 class Main extends React.Component {
+  // keeps the state of the selected hours/time blocks within the ScheduleSelector
   state = { schedule: [] };
 
+  // function to handle change: when a new schedule is made (i.e., an edit is made by the user),
+  // update the internal state to match the new schedule
   handleChange = newSchedule => {
     this.setState({ schedule: newSchedule });
   };
 
+
+  // function that tells React how to render the component
   render() {
+    // log for debugging purposes the internal state of the schedule
     console.log(this.state.schedule);
+    // return a Schedule Selector component with the stored state as the user-selected times,
+    // the number of possible days as 5 (a business week), with each day going from 7am to 10pm,
+    // displayed with the specified date format, and with changes recorded by the handleChange helper function
     return (
       <ScheduleSelector
         selection={this.state.schedule}
@@ -23,4 +34,5 @@ class Main extends React.Component {
   } 
 }
 
+// export this class so we can use it in index.js
 export default Main;
--- when2meet/src/index.js
+++ when2meet/src/index.js
@@ -1,10 +1,16 @@
+// Default generated code for the homepage of the website
+
+// Import relevant React libraries and the Main component (which is the When2Meet table)
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
 
+// The app simply returns an instance of the Main component (which is the When2Meet table)
 function App() {
   return <Main />;
 }
 
+// Get the HTML root (the top-most level)
 const rootElement = document.getElementById("root");
+// and render the application!
 ReactDOM.render(<App />, rootElement);
